b38f384b3747ec0aad4c6dafa590191c56582826
久保優花 <kuboyuka@kuboyukanoMacBook-Pro.local>
2022-05-20 22:34:27 +0900

    flashcard

--- app/controllers/workbooks_controller.rb
+++ app/controllers/workbooks_controller.rb
@@ -20,109 +20,158 @@ class WorkbooksController < ApplicationController
                 @record.save
                 @except = Post.select{ |post| post.id != 166 || post.id != 165}
                 @randomwords = @except.sample(10)
-                @randomwords.each do |randomword|
+                @randomwords.each_with_index do |randomword, idx|
+                    idx += 1
                     @randomword = randomword.word
-                        @postbook = PostBook.new(post_id: randomword.id,workbook_id: @workbook.id).save
+                        @postbook = PostBook.new(post_id: randomword.id,workbook_id: @workbook.id,question_id: idx).save
                 end
             end
     end
 
     def new_flashcard
-        @count = 1
-        @question = 0
-        $id = params[:id]
-        @postbooks = PostBook.where(workbook_id: params[:id]) #10問持ってきてる
-        @posts = Post.where(id: @postbooks.map { |postbook| postbook.post_id }.split(","))
-        @post = @posts[@question]
-        @id_update = Workbook.find_by(id: params[:id])
-        @id_update.update(question_id: 1)
-        @sortid = PostBook.find_by(workbook_id: params[:id],post_id:@post.id)
-        @sortid.update(question_id: @count)
-        if  @posts[@question].synonyms.exists?
-            @synonyms = @posts[@question].synonyms
-            @synonym = @synonyms.pluck(:synonym)
+        @question = PostBook.find_by(workbook_id: params[:id],question_id: 1)
+        @count = @question.question_id
+        @word = Post.find_by(id: @question.post_id)
+        if  @word.synonyms.exists?
+            @synonym = @word.synonyms.pluck(:synonym)
+        else
+            ""
         end
-        @except = Post.select{ |post| post[:mean] != @post.mean }
+        @except = Post.select{ |post| post[:mean] != @word.mean }
         @choices = @except.sample(2)
         @choice = @choices.pluck(:mean)
-        @array = [@choice[0], @choice[1],@post.mean].shuffle
+        @array = [@choice[0], @choice[1],@word.mean].shuffle
         @button = "次の問題へ"
     end
 
     def new_flashcard_js
-            @question =  Workbook.find_by(id: params[:id]).question_id 
-            @count = @question + 1
-            @postbooks = PostBook.where(workbook_id: params[:id]) #10問持ってきてる
-            @posts = Post.where(id: @postbooks.map { |postbook| postbook.post_id }.split(","))
-            @post = @posts[@question]
-            if  @posts[@question].synonyms.exists?
-                @synonyms = @posts[@question].synonyms
-                @synonym = @synonyms.pluck(:synonym)
+        @answer_memory = PostBook.find_by(workbook_id: params[:id],question_id:params[:question_id])
+        @answer_memory.update(answer: params[:answer])
+
+        judge_answer
+
+        @count = params[:question_id].to_i
+        if @count < 10
+            if params[:commit] ==  "次の問題へ"
+                @count += 1
+            elsif   params[:commit] == "前の問題へ"
+                @count -= 1
             end
-            @answer_memory = @postbooks.find_by(post_id: @posts[@question-1].id) 
-            @answer_memory.update(answer: params[:answer])
-            if  @answer_memory.answer == Post.find_by(id: @answer_memory.post_id).mean
-                @answer_memory.update(judgment: "正解")
-            elsif @answer_memory.answer == nil
-                @answer_memory.update(judgment: "未回答")
-            else
-                @answer_memory.update(judgment: "不正解")
-            end
+        end
+
+        p @count
+
+        @question = PostBook.find_by(workbook_id: params[:id],question_id: @count)
+        @word = Post.find_by(id: @question.post_id)
+        if  @word.synonyms.exists?
+            @synonym = @word.synonyms.pluck(:synonym)
+        else
+            ""
+        end
+        @except = Post.select{ |post| post[:mean] != @word.mean }
+        @choices = @except.sample(2)
+        @choice = @choices.pluck(:mean)
+        @array = [@choice[0], @choice[1],@word.mean].shuffle
+
+        if  @count == 10
+            p "----------------"
+            @button = "採点する"
+        else
             @button = "次の問題へ"
-            if  @question == 9
-                @button = "採点する"
-            end
+        end
+
         if  params[:commit] == "次の問題へ"
-            @id_update = Workbook.find_by(id: params[:id])
-            @id_update.update(question_id: @question+1)
-            @answer_memory = @postbooks.find_by(post_id: @posts[@question-1].id) 
+            @answer_memory = PostBook.find_by(workbook_id: params[:id],question_id: params[:question_id])
             @answer_memory.update(answer: params[:answer])
-            if  @answer_memory.answer == Post.find_by(id: @answer_memory.post_id).mean
-                @answer_memory.update(judgment: "正解")
-            elsif @answer_memory.answer == nil
-                @answer_memory.update(judgment: "未回答")
-            else
-                @answer_memory.update(judgment: "不正解")
-            end
-            @post = @posts[@question]
-            @sortid = PostBook.find_by(workbook_id: params[:id],post_id:@post.id)
-            @sortid.update(question_id: @question)
-            if  @posts[@question].synonyms.exists?
-                @synonyms = @posts[@question].synonyms
-                @synonym = @synonyms.pluck(:synonym)
-            end
-            @except = Post.select{ |post| post[:mean] != @posts[@question].mean }
+
+            judge_answer
+
+            @word = Post.find_by(id: @question.post_id)
+        if  @word.synonyms.exists?
+            @synonym = @word.synonyms.pluck(:synonym)
+        else
+            ""
+        end
+            @except = Post.select{ |post| post[:mean] != @word.mean }
             @choices = @except.sample(2)
             @choice = @choices.pluck(:mean)
-            @array = [@choice[0], @choice[1],@posts[@question].mean].shuffle
-        elsif params[:commit] == "前の問題へ"
-            
-            @id_update = Workbook.find_by(id: params[:id])
-            @id_update.update(question_id: @question-1)
-            @post = @posts[@question]
-            if  @posts[@question].synonyms.exists?
-                @synonyms = @posts[@question].synonyms
-                @synonym = @synonyms.pluck(:synonym)
-            end
-            @except = Post.select{ |post| post[:mean] != @posts[@question].mean }
-          p   @choices = @except.sample(2)
-           p  @choice = @choices.pluck(:mean)
-           p  @array = [@choice[0], @choice[1],@posts[@question].mean].shuffle
+            @array = [@choice[0], @choice[1],@word.mean].shuffle
+            p params
+    elsif   params[:commit] == "前の問題へ"
+            p "----------------------------------"
+            @id_update = PostBook.find_by(question_id: params[:id])
+            p @id_update.update(question_id: @question-1)
+            p @post = @posts[@question]
+            judge_synonym
+            choices_answer
+            p "----------------------------------"
+
     elsif   params[:commit] == "採点する"
-            @answer_memory = @postbooks.find_by(post_id: @posts[@question-1].id)
+            @answer_memory = PostBook.find_by(workbook_id: params[:id],question_id: params[:question_id])
             @answer_memory.update(answer: params[:answer])
-            judge
+            
+            judge_answer
+            @postbooks = PostBook.where(workbook_id: params[:id])
             @scoring = @postbooks.where(judgment:"正解").count
             @score = Record.find_by(workbook_id: params[:id])
             @score.update(score: @scoring)
             respond_to do |format|
                 format.js {render ajax_redirect_to( "/workbooks/#{params[:id]}/confirmation")} 
             end
+    else
+        respond_to do |format|
+            format.js {render ajax_redirect_to( "/workbooks/flashcard")} 
         end
+        end
+
     end
 
     def new_flashcard_back
-        
+        workbook_temp
+        @question =  Workbook.find_by(id: params[:id]).question_id
+        i = 0
+        if  params[:commit] ==  "前の問題へ"
+            i  += 1
+        else
+            i = 0
+        end
+        if  i == 1
+            @question -= 2
+        elsif i > 1
+            @question -= 1
+        end
+        @id_update = Workbook.find_by(id: params[:id])
+        @id_update.update(question_id: @question)
+    end
+
+    def judge_synonym
+        if  @posts[@question].synonyms.exists?
+            @synonyms = @posts[@question].synonyms
+            @synonym = @synonyms.pluck(:synonym)
+        else
+            ""
+        end
+    end
+    
+    def judge_answer
+        if  @answer_memory.answer == Post.find_by(id: @answer_memory.post_id).mean
+            @answer_memory.update(judgment: "正解")
+        elsif @answer_memory.answer == nil
+            @answer_memory.update(judgment: "未回答")
+        else
+            @answer_memory.update(judgment: "不正解")
+        end
+    end
+
+    def choices_answer
+        @except = Post.select{ |post| post[:mean] != @posts[@question].mean }
+            @choices = @except.sample(2)
+            @choice = @choices.pluck(:mean)
+            @array = [@choice[0], @choice[1],@posts[@question].mean].shuffle
+    end
+
+    def workbook_temp
+        @postbooks = PostBook.where(workbook_id: params[:id])
     end
 
     def confirmation
@@ -135,19 +184,20 @@ class WorkbooksController < ApplicationController
     end
 
     def continue
-        @count = params[:value].to_i
-        @question = @count - 1
-        @postbooks = PostBook.where(workbook_id: $id) #10問持ってきてる
-        @posts = Post.where(id: @postbooks.map { |postbook| postbook.post_id }.split(","))
-        @post = @posts[@question]
-        @id_update = Workbook.find_by(id: $id)
-        @id_update.update(question_id: @count) 
-        @synonyms = @posts[@question].synonyms
-        @synonym = @synonyms.pluck(:synonym)
-        @except = Post.select{ |post| post[:mean] != @post.mean }
+        @questions = PostBook.where(workbook_id: params[:id],answer: nil)
+        @question = @questions.min
+        @count = @question.question_id
+        workbook_temp #10問持ってきてる
+        @word = Post.find_by(id: @question.post_id)
+        if  @word.synonyms.exists?
+            @synonym = @word.synonyms.pluck(:synonym)
+        else
+            ""
+        end
+        @except = Post.select{ |post| post[:mean] != @word.mean }
         @choices = @except.sample(2)
         @choice = @choices.pluck(:mean)
-        @array = [@choice[0], @choice[1],@post.mean].shuffle
+        @array = [@choice[0], @choice[1],@word.mean].shuffle
         @button = "次の問題へ"
     end
 end
